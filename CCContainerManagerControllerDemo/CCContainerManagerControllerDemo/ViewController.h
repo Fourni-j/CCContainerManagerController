0a6e9916244ea20333dcd8fd3b3dc751f89d7b04
Charles-Adrien Fournier <Charladfr@icloud.com>
2015-06-15 14:57:12 +0200

    Demo project

---
+++ CCContainerManagerControllerDemo/CCContainerManagerControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCContainerManagerControllerDemo
+//
+//  Created by Charles-Adrien Fournier on 15/06/15.
+//  Copyright (c) 2015 Charles-Adrien Fournier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CCContainerManagerControllerDemo/CCContainerManagerControllerDemo/ViewController.m
@@ -0,0 +1,68 @@
+//
+//  ViewController.m
+//  CCContainerManagerControllerDemo
+//
+//  Created by Charles-Adrien Fournier on 15/06/15.
+//  Copyright (c) 2015 Charles-Adrien Fournier. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "CCContainerManagerController.h"
+#import <CCContainerViewController.h>
+
+#define SYSTEM_VERSION_LESS_THAN(v) ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    UIViewController *vc1 = [[UIViewController alloc] init];
+    CCBarItem *item1 = [[CCBarItem alloc] initWithTitle:@"vc1" image:[UIImage imageNamed:@"user"]];
+    UITabBarItem *barItem1 = [[UITabBarItem alloc] initWithTitle:@"vc1" image:[UIImage imageNamed:@"user"] selectedImage:nil];
+    vc1.view.backgroundColor = [UIColor greenColor];
+    vc1.barItem = item1;
+    vc1.tabBarItem = barItem1;
+    
+    UIViewController *vc2 = [[UIViewController alloc] init];
+    CCBarItem *item2 = [[CCBarItem alloc] initWithTitle:@"vc2" image:[UIImage imageNamed:@"camera"]];
+    UITabBarItem *barItem2 = [[UITabBarItem alloc] initWithTitle:@"vc2" image:[UIImage imageNamed:@"camera"] selectedImage:nil];
+    vc2.view.backgroundColor = [UIColor redColor];
+    vc2.barItem = item2;
+    vc2.tabBarItem = barItem2;
+    
+    
+    UIViewController *vc3 = [[UIViewController alloc] init];
+    CCBarItem *item3 = [[CCBarItem alloc] initWithTitle:@"vc3" image:[UIImage imageNamed:@"mail"]];
+    UITabBarItem *barItem3 = [[UITabBarItem alloc] initWithTitle:@"vc3" image:[UIImage imageNamed:@"mail"] selectedImage:nil];
+    vc3.view.backgroundColor = [UIColor yellowColor];
+    vc3.barItem = item3;
+    vc3.tabBarItem = barItem3;
+    
+    CCContainerManagerController *containerManagerController;
+    
+    if (SYSTEM_VERSION_LESS_THAN(@"8.0")) {
+        containerManagerController = [[CCContainerManagerController alloc] init];
+    }
+    else {
+        containerManagerController = [[CCContainerManagerController alloc] initWithTraitCollection:self.traitCollection];
+    }
+    
+    [containerManagerController setViewControllers:[[NSArray alloc] initWithObjects:vc1, vc2, vc3, nil]];
+    [containerManagerController setSelectedIndex:0];
+        
+    [self addChildViewController:containerManagerController];
+    [self.view addSubview:containerManagerController.view];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
